db5b797c3c9f8c3bfd80b68a239477e6f93700a7
obieuan <obieuan@gmail.com>
2022-03-14 18:20:24 -0600

    update

---
+++ _printf.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_numbers -
+ * @format:
+ * Return:
+ */
+int _printf(const char *format, ...)
+{
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stdio.h>
+int _printf(const char *format, ...);
+
+#endif
