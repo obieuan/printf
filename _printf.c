e59f97c16019c5d823a31a65e8ab58c98836f457
obieuan <obieuan@gmail.com>
2022-03-15 15:03:04 -0600

    update

--- _printf.c
+++ _printf.c
@@ -6,4 +6,27 @@
  */
 int _printf(const char *format, ...)
 {
+    int i = 0, contador = 0;
+    
+    if (format == NULL)
+    {
+        return (-1);
+    }
+    va_list lista;
+    int (*lista_nueva)(va_list);
+
+    while (format[i])
+    {
+        for (i = 0; format[i] != '%' && format[i]; i++)
+        {
+            putchar(format[i]);
+            contador++;
+        }
+        if(!format[i])
+        {
+            return (contador);
+        }
+        
+    }
+    
 }
